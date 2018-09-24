f46da934190a92c2a5caebe14084134c49a5eb27
Baartokk <43533052+Baartokk@users.noreply.github.com>
2018-09-24 15:57:25 +0200

    Create simple-file-merger.py

---
+++ simple-file-merger.py
@@ -0,0 +1,21 @@
+import os, time
+
+path = 'files_to_merge/' #cesta k suborom
+file_format = 'txt' #format kopirovanych suborov a vysledneho suboru
+header = False #ci maju subory hlavicku
+
+#---------------------------------------------------------------------
+
+list_of_files = [item for item in os.listdir(path) if '.{0}'.format(file_format) in item]
+
+print('Merging...')   
+ 
+with open('000_merge_{0}.{1}'.format(time.strftime("%Y-%m-%d_%H%M%S"), file_format), 'w', encoding = '1250', newline = '') as newFile:
+    for idx, file in enumerate(list_of_files):            
+        with open(path + file, 'r', encoding = '1250', newline = '') as rFile:
+            
+            rFile.readline() if idx > 0 and header else 0
+            for line in rFile:
+                newFile.write(line)
+
+print('Done.\nFiles merged: {0}'.format(len(list_of_files)))
